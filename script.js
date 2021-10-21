4d958bac027807aa276ed1b3b10ace489c6ac04d
Polina <p.artamonova@gmail.com>
2021-10-21 21:30:18 +0200

    scroll works

--- script.js
+++ script.js
@@ -13,67 +13,48 @@ async function start() {
   setTopRight(topRightBox);
   const bottomRightBox = document.querySelector("#svgBottomRight svg");
   setBottomRight(bottomRightBox);
-
-  // getPositions(topLeftBox, topRightBox, bottomRightBox);
-  // const lines = document.querySelector("#lines");
 }
-
-// let newPath = document.createElement("path");
-
-// // newPath.setAttribute("d", "M470 160 H1300")
 
 function SetTopLeft(shape) {
   shape.setAttribute("width", "100%");
   shape.setAttribute("height", "100%");
   shape.setAttribute("viewBox", "0 0 900 300");
+  shape.setAttribute("class", "svgToChange");
 }
 function setTopRight(shape) {
   shape.setAttribute("width", "100%");
   shape.setAttribute("height", "100%");
   shape.setAttribute("viewBox", "0 0 300 300");
+  shape.setAttribute("class", "svgToChange");
 }
 
 function setBottomRight(shape) {
   shape.setAttribute("width", "100%");
   shape.setAttribute("height", "100%");
   shape.setAttribute("viewBox", "0 0 1000 400");
+  shape.setAttribute("class", "svgToChange");
 }
+gsap.registerPlugin(ScrollTrigger);
 
-// function getPositions(topLeft, topRight, bottomRight) {
-//   const pathStartHor = topLeft.getBoundingClientRect();
-//   // console.log("horizontal path start", pathStartHor);
-//   const pathEndHor = topRight.getBoundingClientRect();
-//   // console.log("Horizontal path end", pathEndHor);
-//   const pathStartVer = topRight.getBoundingClientRect();
-//   // console.log("vertical path start", pathStartVer);
-//   const pathEndVer = bottomRight.getBoundingClientRect();
-//   // console.log("vertical path end", pathEndVer);
-//   drawPath(pathStartHor, pathEndHor, pathStartVer, pathEndVer);
-// }
+gsap.to(".panel:not(:last-child)", {
+  yPercent: -300,
+  ease: "none",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#scrollSection",
+    start: "top top",
+    end: "+=300%",
+    scrub: true,
+    pin: true,
+  },
+});
 
-// function drawPath(pos1, pos2, pos3, pos4) {
-//   console.log(
-//     "position1",
-//     pos1,
-//     "position2",
-//     pos2,
-//     "position3",
-//     pos3,
-//     "position4",
-//     pos4
-//   );
-//   // const newPath = document.querySelector("#lines path");
-//   // newPath.setAttributeNS(
-//   //   null,
-//   //   "d",
-//   //   `M${Math.round(pos1.right)} ${Math.round(pos1.bottom)} H${Math.round(
-//   //     pos2.left
-//   //   )}`
-//   // );
-//   // newPath.setAttributeNS(null, "stroke", "red");
-//   // newPath.setAttributeNS(null, "fill", "none");
-//   // newPath.setAttribute("d", `M${pos1.right} ${pos1.bottom} H${pos2.left}`);
-//   // newPath.setAttribute("stroke", "red");
-//   // newPath.setAttribute("stroke-width", "1");
-//   // console.log(newPath);
-// }
+gsap.set(".panel", { zIndex: (i, target, targets) => targets.length - i });
+
+window.addEventListener("scroll", onScroll);
+
+function onScroll() {
+  console.log("scrolling");
+  let svg = document.querySelectorAll("#svgTopLeft ");
+  // console.log(svg);
+}
